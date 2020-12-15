2c96b7d61c1dc029fd9bf62d6d91feab1f175cbd
kentgeek <86157540@qq.com>
2020-12-15 22:33:50 +0800

    更改秘钥储存方式

--- dprkshow_vue/settings.py
+++ dprkshow_vue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secrects import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -71,6 +72,9 @@ WSGI_APPLICATION = 'dprkshow_vue.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRET_KEY
 
 DATABASES = {
     'default': {
